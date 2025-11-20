9f791d8b2d6ff376febfe88ad1db29de5aefd02a
Kevin David <kevindavidvenyk@gmail.com>
2025-11-20 20:50:14 +0200

    app

--- Udemy-app/src/App.jsx
+++ Udemy-app/src/App.jsx
@@ -4,7 +4,7 @@ import OfferCard from "./Components/OfferCard"
 import SkillsSection from "./Components/SkillsSection"
 import ReimagineSection from './Components/ReimagineSection'
 import TopicsSection from './Components/TopicsSection'
-import TrustedSection from './Components/TrustedSection'
+import TrustedSection from './Components/TrustedSection.jsx'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 
